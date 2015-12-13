c390bd50d24927a69fc3b5de1e0e65a542f3b14a
Albert Sun <albert.sun@purestorage.com>
2015-12-12 23:25:23 -0800

    Q.22

--- 22.py
+++ 22.py
@@ -1,3 +1,16 @@
-with open("../22.txt") as f:
-    content = f.readline()
-print(content)
+names = None
+
+with open("data/22.txt") as f:
+    names = f.readline()
+
+names = names.split(',')
+names = [n.strip('"') for n in names]
+names.sort()
+
+ret = 0
+
+for i in range(len(names)):
+    score = (i + 1) * sum([ord(c) - ord('A') + 1 for c in list(names[i])])
+    ret += score
+
+print(ret)
